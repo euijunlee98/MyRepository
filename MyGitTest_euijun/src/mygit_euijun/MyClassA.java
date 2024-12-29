c0339a210b52c3801975e3fc3ac0edd82add79c7
euijun <euijun@localhost>
2024-12-29 21:34:23 +0900

    Add a println line

--- MyGitTest_euijun/src/mygit_euijun/MyClassA.java
+++ MyGitTest_euijun/src/mygit_euijun/MyClassA.java
@@ -3,5 +3,6 @@ package mygit_euijun;
 public class MyClassA {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("This is the 2nd version.");
 	}
 }
