b04404d096446fc38494b5b7aa3c5182e92bbe49
euijun <euijun@localhost>
2024-12-29 21:45:46 +0900

    First commit in devB branch

--- MyGitTest_euijun/src/mygit_euijun/MyClassA.java
+++ MyGitTest_euijun/src/mygit_euijun/MyClassA.java
@@ -4,5 +4,7 @@ public class MyClassA {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version.");
+		YourClassA yourA = new YourClassA();
+		yourA.sayHello("Everyone");
 	}
 }
